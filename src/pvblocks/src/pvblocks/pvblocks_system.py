cc9e37a7e709d12dcf5eb61823750e9d3b7fb14c
Erik Haverkamp <erik.haverkamp@rerasolutions.com>
2025-11-21 13:16:19 +0100

    pvblocks-python v0.1.3

--- src/pvblocks/src/pvblocks/pvblocks_system.py
+++ src/pvblocks/src/pvblocks/pvblocks_system.py
@@ -23,8 +23,10 @@ def ReadSerial(ser):
 class PvBlocks(object):
     TYPES = {
         20: 'IV/MPP IV-Curve control and measure PvBlock',
+        27: 'IV/MPP IV-Curve control and measure PvBlock',
         30: 'PV-IRR 4x analog voltage readout block',
         40: 'PV-TEMP 4x Pt100 readout block',
+        41: 'PV-TEMP 2x Thermocouple T readout block',
         50: 'PV-MOD digital modbus module'
     }
 
@@ -39,6 +41,7 @@ class PvBlocks(object):
         self.Blocks = []
         self.IvMppBlocks = []
         self.PvIrrBlocks = []
+        self.PvBaseSystemType = constants.RR1700
 
     def init_system(self):
         self.uart.write(serial.to_bytes([1, constants.ALIVE]))
@@ -46,7 +49,11 @@ class PvBlocks(object):
         bts = ReadSerial(self.uart)
         if len(bts) != 2:
             raise exceptions.NoResponseException()
-        return bts[0] == 3 and bts[1] == constants.ALIVE
+
+        if bts[1] == constants.ALIVE_RR1701:
+            self.PvBaseSystemType = constants.RR1701
+
+        return bts[0] == 3 and bts[1] == constants.ALIVE or bts[1] == constants.ALIVE_RR1701
 
 
     def close_system(self):
@@ -69,6 +76,9 @@ class PvBlocks(object):
             match bts[(index * 9) + 4 + 8]:
                 case 20:
                     blck = IvMpp(bts[(index * 9) + 4: (index * 9) + 13], self.uart)
+                    self.IvMppBlocks.append(blck)
+                case 27:
+                    blck = IvMpp27(bts[(index * 9) + 4: (index * 9) + 13], self.uart)
                     self.IvMppBlocks.append(blck)
                 case 30:
                     blck = PvIrr(bts[(index * 9) + 4: (index * 9) + 13], self.uart)
@@ -95,6 +105,7 @@ class PvBlock(object):
         self.Guid = uuid.UUID(int=id)
         self.Type = bytes[8]
         self.uart = uart
+        self.node = 2
 
     def open(self):
         self.uart.write(serial.to_bytes([1,
@@ -132,7 +143,7 @@ class PvBlock(object):
 
     def read_statusbyte(self):
         self.open()
-        self.uart.write(serial.to_bytes([2, constants.GET_STATUS]))
+        self.uart.write(serial.to_bytes([self.node, constants.GET_STATUS]))
         sleep(0.5)
         bts = ReadSerial(self.uart)
         self.close()
@@ -144,6 +155,8 @@ class PvBlock(object):
         status = self.read_statusbyte()
         d = {'firmware': status.firmware, 'hardware': status.hardware}
         return d
+
+
 
 
 class IvMpp(PvBlock):
@@ -268,6 +281,170 @@ class IvMpp(PvBlock):
         bts = ReadSerial(self.uart)
         self.close()
         return bts[3:]
+
+
+class IvMpp27(PvBlock):
+    def __init__(self, bytes, uart):
+        super().__init__(bytes, uart)
+        self.node = bytes[1]
+
+    def reset(self):
+        self.open()
+        self.uart.write(serial.to_bytes([self.node, constants.SELF_RESET_CMD]))
+        sleep(0.5)
+        self.close()
+
+    def read_ivpoint(self):
+
+        self.open()
+        self.uart.write(serial.to_bytes([self.node, constants.READ_COMMAND]))
+        sleep(0.5)
+        bts = ReadSerial(self.uart)
+        self.close()
+        if len(bts) < 15:
+            raise exceptions.UnexpectedResponseException()
+
+        if bts[2] != 12:
+            raise exceptions.UnexpectedResponseException()
+
+        r1 = int.from_bytes(bts[3:7], "little") / 10000.0
+        r2 = int.from_bytes(bts[7:11], "little") / 100000.0
+        ivpoint = IvPoint(r1, r2)
+
+        return ivpoint
+
+    def ApplyVoc(self):
+        self.open()
+        self.uart.write(serial.to_bytes([self.node, constants.IDLE_COMMAND]))
+        sleep(0.5)
+        self.close()
+
+
+    def ApplyMpp(self):
+        self.open()
+        self.uart.write(serial.to_bytes([self.node, constants.MPP_COMMAND]))
+        sleep(0.5)
+        self.close()
+
+    def ApplyIsc(self):
+        voltage = 0.0;
+        self.open()
+        bytes = list(((int)(1000 * voltage)).to_bytes(4, "little"))
+        self.uart.write(serial.to_bytes([self.node, constants.VOLTAGE_COMMAND, bytes[0], bytes[1], bytes[2], bytes[3]]))
+        sleep(0.5)
+        self.close()
+
+    def ApplyVoltageBias(self, voltage):
+        self.open()
+        bytes = list(((int)(1000 * voltage)).to_bytes(4, "little"))
+        self.uart.write(
+            serial.to_bytes([self.node, constants.VOLTAGE_COMMAND, bytes[0], bytes[1], bytes[2], bytes[3]]))
+        sleep(0.5)
+        self.close()
+
+    def measure_ivcurve(self, points, delay_ms, sweepstyle):
+        self.open()
+
+        self.uart.write(
+            serial.to_bytes([self.node, constants.SET_TRIGGER_COMMAND, 0]))
+
+        sleep(0.5)
+
+        self.uart.write(
+            serial.to_bytes([self.node, constants.CURVE_COMMAND, points, delay_ms, 0, 0, 0, 0, sweepstyle]))
+
+        while self.uart.inWaiting() != 3:
+            sleep(0.01)
+
+        bts = ReadSerial(self.uart)
+
+        status = self.read_statusbyte()
+        while status.mode == 5:
+            sleep(0.5)
+            status = self.read_statusbyte()
+
+        self.close()
+        points_measured = status.statusbytes[0]
+        curve = self.transfer_curve(points_measured)
+
+        return curve
+
+    def transfer_curve(self, points):
+        self.open()
+        self.uart.write(serial.to_bytes([self.node, constants.TRANSFER_CURVE_COMMAND]))
+        sleep(0.5)
+        availablebytes = 8 + (points * 8) + 1
+        toread = self.uart.inWaiting()
+        while toread != availablebytes:
+            toread = self.uart.inWaiting()
+            print(toread)
+            sleep(0.1)
+        bts = ReadSerial(self.uart)
+        self.close()
+
+        voltages = []
+        currents = []
+
+        for i in range(1, int((availablebytes - 1)/8)):
+            index = (i * 8) + 1
+            voltages.append(int.from_bytes(bts[index:(index+4)], "little") / 10000.0)
+            index = index + 4
+            currents.append(int.from_bytes(bts[index:(index+4)], "little") / 100000.0)
+
+        return {'voltages': voltages, 'currents': currents}
+
+    def read_calibration(self):
+        c = {'A': 0.0, 'B': 0.0, 'C': 0.0, 'D': 0.0}
+        bts = self.read_eeprom(4, 16)
+
+        c['A'] = struct.unpack('<f', bytearray(bts[0:4]))
+        c['B'] = struct.unpack('<f', bytearray(bts[4:8]))
+        c['C'] = struct.unpack('<f', bytearray(bts[8:12]))
+        c['D'] = struct.unpack('<f', bytearray(bts[12:16]))
+
+        return c
+
+    def write_voltage_calibration(self, A, B):
+        ba = list(bytearray(struct.pack("<f", A)))
+        self.write_eeprom(ba, 4)
+        ba = list(bytearray(struct.pack("<f", B)))
+        self.write_eeprom(ba, 8)
+        self.reset()
+        sleep(3)
+
+
+    def write_current_calibration(self,C, D):
+        ba = list(bytearray(struct.pack("<f", C)))
+        self.write_eeprom(ba, 12)
+        ba = list(bytearray(struct.pack("<f", D)))
+        self.write_eeprom(ba, 16)
+        self.reset()
+        sleep(3)
+
+    def read_eeprom(self, address, length):
+        bts = list(address.to_bytes(self.node, 'little'))
+        self.open()
+        self.uart.write(
+            serial.to_bytes([self.node, constants.READ_EEPROM_COMMAND, length, bts[0], bts[1]]))
+
+        while self.uart.inWaiting() != length + 3:
+            sleep(0.01)
+
+        bts = ReadSerial(self.uart)
+        self.close()
+        return bts[3:]
+
+    def write_eeprom(self, data, address):
+        bts = list(address.to_bytes(self.node, 'little'))
+        tx = [self.node, constants.WRITE_EEPROM_COMMAND, len(data), bts[0], bts[1]]
+
+        self.open()
+        self.uart.write(serial.to_bytes(tx + data))
+        self.close()
+        sleep(0.5)
+
+
+
 
 
 class PvIrr(PvBlock):
