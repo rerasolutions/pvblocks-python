72174210d0f288fe0efc37c1179f2e461b290fda
erikhaverkamp <erik@rera.nl>
2026-01-17 11:57:45 +0100

    Added label writing

--- src/pvblocks/src/test_api.py
+++ src/pvblocks/src/test_api.py
@@ -1,15 +1,19 @@
-import sys
+host = '100.105.180.7'
+apikey = '4a8bd150-fa41-4fef-a079-f1a0736d6bba'
 
 from pvblocks import pvblocks_api
-from src.pvblocks.src.pvblocks.pvblocks_api import get_channel_number
 
 print(pvblocks_api.show_version())
-pvblocks = pvblocks_api.PvBlocksApi('100.105.180.7', '4a8bd150-fa41-4fef-a079-f1a0736d6bba')
+pvblocks = pvblocks_api.PvBlocksApi(host ,apikey)
 pvblocks.Init()
 
 for b in pvblocks.Blocks:
     print(b['label'])
     if b['type'] == "RR-1727":
-        channel = get_channel_number(b['usbNr'], b['boardNr'], b['channelNr'])
+        channel = pvblocks_api.get_channel_number(b['usbNr'], b['boardNr'], b['channelNr'])
         label = "IVMPP-{}".format(channel)
+        pvblocks.write_block_label(b['id'], label)
+    if b['type'] == "RR-1741":
+        location = 4* b['usbNr'] + b['boardNr'] + 1
+        label = "IVTEMP-{}".format(location)
         pvblocks.write_block_label(b['id'], label)
