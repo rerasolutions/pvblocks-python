7e7807b5985bc07d65c13514b1e112063034c010
erikhaverkamp <erik.haverkamp@rerasolutions.com>
2026-05-03 15:48:40 +0200

    Added correct version number Added testfile

--- src/pvblocks/tests/set_manual_time.py
+++ src/pvblocks/tests/set_manual_time.py
@@ -1,8 +1,8 @@
 from pvblocks import pvblocks_api
 from datetime import datetime, timezone
 
-host = '100.110.124.61' #ip address of pvblocks system
-apikey = 'bc3e9db9-c249-4dd6-a631-927ec85d8bdd' #API key as found in Data/PVBlocks API
+host = '111.111.111.111' #ip address of pvblocks system
+apikey = 'aaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa' #API key as found in Data/PVBlocks API
 
 print(pvblocks_api.show_version())
 pvblocks = pvblocks_api.PvBlocksApi(host ,apikey)
